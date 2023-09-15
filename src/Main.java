87622736eac0e7b1593629be425949acccd57480
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-15 21:13:59 +0300

    Completed version

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         double purchasePrice = 100.00; // Replace with the actual purchase price
-        final double TAX_RATE = 0.05;   // 5% sales tax rate
+        final double AX_RATE = 0.05;   // 5% sales tax rate
 
         double salesTaxAmount = purchasePrice * TAX_RATE;
 
